6d0ed30ce7219de828a71a73441534d108b98902
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2022-05-10 14:20:06 +0100

    setup the login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import{Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Personnel from "./components/Personnel";
 import Business from "./components/Business";
+import Login from './components/login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Personnel />} />
           <Route path="/Business" element={<Business />} />
+          <Route path="/login" element={<Login />} />
         </Routes> 
       </main>      
     </div>
--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
   return (
-    
+    <div className='App'>
+        <div className="container login-page">
+            <div className="row">
+                <div className="login-form card g-2">
+                    <h2 className='text-center'>Welcome to Internet Banking</h2>
+                    <form action="POST">
+                        <div className='form-user'>
+                            <label for="Username">Username:</label>
+                            <input type="text" name="username" />
+                        </div>
+                        <div className='form-password'>
+                            <label for="Password">Password:</label>
+                            <input type="password" name="password" />
+                        </div>
+                        <hr />
+                        <NavLink to="#">Forgotten your login details?</NavLink>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
